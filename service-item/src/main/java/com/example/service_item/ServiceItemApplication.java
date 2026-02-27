302872eca795a62a544abb69fe6d6e0b9142caef
hyeonsang010716 <kyr76789@gmail.com>
2026-02-27 17:11:37 +0900

    feat: item server init

---
+++ service-item/src/main/java/com/example/service_item/ServiceItemApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceItemApplication.class, args);
+	}
+
+}
